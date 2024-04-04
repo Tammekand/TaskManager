3f0c963a9699e060cf3dd88368d58aa6616ae4ec
Axel-Martin Tammekand <axelmartintammekand@gmail.com>
2024-04-04 23:48:33 +0300

    first prototype

---
+++ TodoItem.cs
@@ -0,0 +1,8 @@
+namespace TodoApp;
+
+public class TodoItem
+{
+    public string? Description { get; set; }
+    public DateTime? DueDate { get; set; }
+    public DateTime? CreatedDate { get; set; }
+}
